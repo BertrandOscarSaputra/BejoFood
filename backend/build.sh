6e8f2b104f3ad3d0a63b435b97445936049f4b33
BertrandOscarSaputra <bertrandosc@gmail.com>
2026-01-05 17:09:36 +0800

    Remove build.sh - let Koyeb handle everything

--- backend/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Build script for Koyeb deployment
-# Note: pip install is handled by Koyeb buildpack, collectstatic too
-
-set -o errexit
-
-# Run database migrations
-python manage.py migrate --noinput
